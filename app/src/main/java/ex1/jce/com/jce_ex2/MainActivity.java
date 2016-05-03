ddde746bc0d6c1a41d1077c7aab1008c2a6afc65
Muhammad Ne <muhammad.ne1@gmail.com>
2016-05-03 14:26:49 +0300

    Comments, Organizing code

--- app/src/main/java/ex1/jce/com/jce_ex2/MainActivity.java
+++ app/src/main/java/ex1/jce/com/jce_ex2/MainActivity.java
@@ -42,28 +42,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private GoogleApiClient googleApiClient; // Object that is used to connect to google maps API, must be built to use fused location
     private LocationRequest locationRequest; //Object that requests a quality of service for location updates from fused location
-    private Location oldLocation = null;
+    private Location oldLocation = null; // Var for checking if the new location is > 1m than the old location
 
-    final private int PERMISSION_REQUEST_CODE = 1;
+    final private int PERMISSION_REQUEST_CODE = 1; // request code for permissions
 
-    DatabaseHandler db;
-    Cursor cursor;
-    CustomCursorAdapter customCursorAdapter;
+    // Declaring objects for database and cursor
+    private DatabaseHandler db;
+    private Cursor cursor;
+    private CustomCursorAdapter customCursorAdapter;
 
-    ListView listView;
-    Button button;
-    Button searchB;
-    TextView addressTV;
-
-    EditText latED, lngED, timeED;
+    // Declaring views
+    private ListView listView;
+    private Button clearB, searchB;
+    private TextView addressTV;
+    private EditText latED, lngED, timeED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initializing views
         listView = (ListView) findViewById(R.id.listView);
-        button = (Button) findViewById(R.id.deleteQuery);
+        clearB = (Button) findViewById(R.id.deleteQuery);
         searchB = (Button) findViewById(R.id.search);
         addressTV = (TextView) findViewById(R.id.addressTV);
         latED = (EditText) findViewById(R.id.latED);
@@ -77,52 +78,34 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 .addOnConnectionFailedListener(this)
                 .build();
 
+        // Build database
         db = new DatabaseHandler(this);
 
-        db.addLocation(new LocationData("14.1111", "14.1112", "01-02-2016"));
-        db.addLocation(new LocationData("14.2221", "14.2222", "01-02-2016"));
-        db.addLocation(new LocationData("23.1111", "23.2222", "04-05-2016"));
-        db.addLocation(new LocationData("23.2333", "24.4444", "04-05-2016"));
-        db.addLocation(new LocationData("32.6666", "32.6666", "06-04-2016"));
-        db.addLocation(new LocationData("32.7777", "32.8888", "07-04-2017"));
-        db.addLocation(new LocationData("14.22345", "15.123123", "07-04-2017"));
-        //db.addLocation(new LocationData("", "", ""));
-
-
-        /**
-         * CRUD Operations
-         * */
-
+        // Get records of saves location, save them in cursor and bind them to listview
         cursor = db.getAllLocations();
-        while (cursor.moveToNext()) {
-            System.out.print(cursor.getString(0) + " / ");
-            System.out.print(cursor.getString(1) + " / ");
-            System.out.print(cursor.getString(2) + " / ");
-            System.out.println(cursor.getString(3));
-
-        }
-
         customCursorAdapter = new CustomCursorAdapter(getApplicationContext(), cursor);
         listView.setAdapter(customCursorAdapter);
-        listView.setOnItemClickListener(this);
+        listView.setOnItemClickListener(this); // Define onItemClickListener for listView
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // Button listener for deleting all records from database : this will drop the entire database and create a new one
+        clearB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                db.emptyTable();
-
-                db = new DatabaseHandler(getApplicationContext());
+                db.emptyTable(); // drop table and create a new one
+                //db = new DatabaseHandler(getApplicationContext());
                 cursor = db.getAllLocations();
                 customCursorAdapter.changeCursor(cursor);
 
             }
         });
 
+        // Button clickListener for search using lat lng time
         searchB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // If at least one field is not empty then apply the search
                 if (!latED.getText().toString().isEmpty() || !lngED.getText().toString().isEmpty() || !timeED.getText().toString().isEmpty()) {
                     cursor = db.searchLocation(latED.getText().toString(), lngED.getText().toString(), timeED.getText().toString());
                     customCursorAdapter.changeCursor(cursor);
@@ -133,24 +116,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
 
+    // This is done using onResume instead of onStart to overRide when the user pauses the app to allow permissions from settings.
+    // Or if user paused the app and gets back to it for any reason.
     protected void onResume() {
 
         super.onResume();
 
         // Check if user granted permissions
         if (checkPermissions()) {
-            //System.out.println("Permission Check : " + access_fine_location_permissionCheck);
-            System.out.println("> Permissions are GRANTED.");
-            System.out.println("> Connecting googleApiClient.");
-            googleApiClient.connect();
+            googleApiClient.connect(); // connect only if user granted permissions
         } else {
             System.out.println("> Permissions are DENIED, Requesting from user : ");
-
             String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET};
             ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
         }
     }
 
+    //  Disconnect apiClient and close cursor
     protected void onPause() {
 
         super.onPause();
@@ -161,12 +143,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         super.onStop();
         googleApiClient.disconnect();
+        cursor.close();
     }
 
 
+    // Function to check permissions
+    // If all permissions are granted then return true;
+    // If false then grant the user to allow permissions
     private boolean checkPermissions() {
-
-        System.out.println("> Checking Permissions : ");
 
         int access_fine_location_permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
         int Internet_permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.INTERNET);
@@ -178,21 +162,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
     }
 
+    // This method as called a result for interacting with the user, check if the user allowed or denied permissions
     public void onRequestPermissionsResult(int request_code, String[] permissions, int[] results) {
         switch (request_code) {
             case PERMISSION_REQUEST_CODE: {
                 if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED && results[1] == PackageManager.PERMISSION_GRANTED) {
 
-                    Log.w("MainActivity", "Permissions Granted");
                     googleApiClient.connect();
                 } else {
-                    Log.e("MainActivity", "Permissions Denied");
                 }
             }
         }
     }
 
 
+    // If permissions are granted, connect the googleApi
     @Override
     public void onConnected(Bundle bundle) {
 
@@ -200,7 +184,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         locationRequest = locationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(5000);
+        locationRequest.setInterval(5000); // Search for new location each 5 seconds
         locationRequest.setFastestInterval(5000);
         LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
     }
@@ -215,17 +199,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
+    // On location changed method
     @Override
     public void onLocationChanged(Location location) {
 
+        // Check if location  != null, if yes then check if old location != null, if yes, then check if the difference between old location and new is more than 1m
         if (location != null) {
 
+            // if oldLocation isnt set then set it as the new location, after that compare with the new location
             if (oldLocation == null) {
                 oldLocation = location;
             } else {
+
+                // check if distance between locations is more than 1M
                 float distanceInMeters = oldLocation.distanceTo(location);
+                Toast.makeText(getApplicationContext(), distanceInMeters + "", Toast.LENGTH_SHORT).show();
                 if (distanceInMeters >= 1.0) {
-                    Toast.makeText(getApplicationContext(), distanceInMeters + "", Toast.LENGTH_SHORT).show();
+                    // Get the time using Calendar class, result is in Millisecond
+                    // Parse the result to get all the data.
+
                     Calendar calendar = Calendar.getInstance();
                     calendar.setTimeInMillis(System.currentTimeMillis());
                     String cYear = Integer.toString(calendar.get(Calendar.YEAR));
@@ -235,66 +227,65 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     String cMinute = Integer.toString(calendar.get(Calendar.MINUTE));
                     String cSecond = Integer.toString(calendar.get(Calendar.SECOND));
 
-
-
-                    db.addLocation(new LocationData(Double.toString(location.getLatitude()), Double.toString(location.getLongitude()),cYear + "-" + cMonth + "-" + cDay
-                    + " " + cHour + ":" + cMinute + ":" + cSecond));
+                    // Add new location to database
+                    db.addLocation(new LocationData(Double.toString(location.getLatitude()), Double.toString(location.getLongitude()), cYear + "-" + cMonth + "-" + cDay
+                            + " " + cHour + ":" + cMinute + ":" + cSecond));
                     cursor = db.getAllLocations();
-                    customCursorAdapter.changeCursor(cursor);
-
-                    System.out.println(location.getLatitude() + " // " + location.getLongitude());
-                    oldLocation = location;
+                    customCursorAdapter.changeCursor(cursor); // refresh adapter
+                    oldLocation = location; // sets the old location as the existing location to compare with the upcoming location
                 } else {
-                    Toast.makeText(getApplicationContext(), distanceInMeters + "", Toast.LENGTH_SHORT).show();
+
                 }
             }
 
         } else {
-            System.out.println("> Location = NULL");
         }
     }
 
+    // Item click listener
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-        cursor = db.getLocation(position + 1);
+        cursor = db.getLocation(position + 1); // position in list is +1 than position in cursor
 
-        cursor.moveToFirst();
+        cursor.moveToFirst(); // make sure that the cursor is pointing to the current cell.
 
-        System.out.print(cursor.getString(0) + " / ");
-        System.out.print(cursor.getString(1) + " / ");
-        System.out.print(cursor.getString(2) + " / ");
-        System.out.println(cursor.getString(3));
-
+        // Asynctask for getting the address from lng and lat
+        // Send params (lat lng)
         GetAddress getAddress = new GetAddress(Double.parseDouble(cursor.getString(1)), Double.parseDouble(cursor.getString(2)));
         getAddress.execute();
-        System.out.println("Position : " + position);
     }
 
-
+    //*******************************************************
+    // INNER ASYNCTASK CLASS
+    //*******************************************************
     class GetAddress extends AsyncTask<Void, Void, String> {
 
+        // Declaring variables
         private double lat;
         private double lng;
         private ProgressDialog progress;
 
+        // Constructor for initializing variables
         public GetAddress(double lat, double lng) {
 
             this.lat = lat;
             this.lng = lng;
         }
 
+        // Method that converts lat lng to address using GEOCODER,
+        // returns list with 1 cell
         public List<Address> getFromLocation(double lat, double lng, int maxResult) throws IOException {
 
             Geocoder geocoder;
             List<Address> addresses;
             geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
-
             addresses = geocoder.getFromLocation(lat, lng, maxResult);
 
             return addresses;
         }
 
+        // onPreExcecute : prepare progress dialog
         protected void onPreExecute() {
 
             progress = new ProgressDialog(MainActivity.this);
@@ -303,16 +294,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             progress.show();
         }
 
+        // onackGround : get location
         @Override
         protected String doInBackground(Void... params) {
             String address_string = "";
             try {
 
-                //getFromLocation(31.803052, 35.211399, 1);
                 if (getFromLocation(lat, lng, 1).size() > 0) {
 
                     Address address = getFromLocation(lat, lng, 1).get(0);
-                    System.out.println("Address : " + address);
+                    System.out.println("Address : " + address); // Bilding the string to set in the textview
                     for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
                         address_string += address.getAddressLine(i).toString() + ", ";
                     }
@@ -321,6 +312,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     }
                     address_string += ".";
                 } else {
+                    // if list is empty then write Unavailable
                     address_string = "Unavailable";
                 }
             } catch (IOException e) {
@@ -329,6 +321,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             return address_string;
         }
 
+        // onPost close progress dialog
         protected void onPostExecute(String content) {
 
             System.out.println("Content onPostExecute : " + content);
